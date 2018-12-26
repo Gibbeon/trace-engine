4a2d3a802197eeb8976183dad62766472a635e4d
Richard Woods <me@rfwoods.net>
2018-12-26 14:56:43 -0600

    moved code into tengine library

--- teditor/src/main.cpp
+++ teditor/src/main.cpp
@@ -1,20 +1,3 @@
-/*#include <cstdio>
-
-int main(int argc, char** argv) {
-    printf("Hello World!");
-}
-*/
-
-//*********************************************************
-//
-// Copyright (c) Microsoft. All rights reserved.
-// This code is licensed under the MIT License (MIT).
-// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
-// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
-// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
-// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
-//
-//*********************************************************
 #include <windows.h>
 
 #include "stdafx.h"
---
+++ tengine/build-win64.py
@@ -0,0 +1,19 @@
+import os, subprocess, shutil
+
+if not os.path.exists("build/win64"):
+	os.makedirs("build/win64")
+
+INIT_ENV_BAT = "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Auxiliary/Build/vcvars64.bat"
+
+vars = subprocess.check_output([INIT_ENV_BAT, '&&', 'set'])
+for var in vars.splitlines():
+	var = var.decode('cp1252')
+	k, _, v = map(str.strip, var.strip().partition('='))
+	if k.startswith('?'):
+		continue
+	os.environ[k] = v
+
+os.chdir("build/win64")
+os.system("cmake ../../ -G \"Visual Studio 15 2017\" -A x64 -T host=x64")
+os.chdir("../../")
+os.system("devenv /build debug ./build/win64/" + os.path.basename(os.getcwd()) + ".sln")
